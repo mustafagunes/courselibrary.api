de75e6f67a163e0034ee67f18d00ea350caa1b8f
Mustafa GUNES <gunes149@gmail.com>
2020-09-10 08:45:04 +0300

    created database

--- CourseLibrary/Startup.cs
+++ CourseLibrary/Startup.cs
@@ -28,8 +28,7 @@ namespace CourseLibrary.API
 
             services.AddDbContext<CourseLibraryContext>(options =>
             {
-                options.UseSqlServer(
-                    @"Server=(localdb)\mssqllocaldb;Database=CourseLibraryDB;Trusted_Connection=True;");
+                options.UseSqlServer(Configuration.GetConnectionString("CourseLibraryDB"));
             }); 
         }
 
