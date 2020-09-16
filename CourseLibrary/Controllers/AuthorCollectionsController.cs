605d099aa6d4f0362de33220315b37e42ca438ba
Mustafa GUNES <gunes149@gmail.com>
2020-09-16 19:08:16 +0300

    created a collection

---
+++ CourseLibrary/Controllers/AuthorCollectionsController.cs
@@ -0,0 +1,37 @@
+using System;
+using System.Collections.Generic;
+using AutoMapper;
+using CourseLibrary.API.Models;
+using CourseLibrary.API.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CourseLibrary.API.Controllers
+{
+    [ApiController]
+    [Route("api/authorcollections")]
+    public class AuthorCollectionsController : ControllerBase
+    {
+        private readonly ICourseLibraryRepository _courseLibraryRepository;
+        private readonly IMapper _mapper;
+
+        public AuthorCollectionsController(ICourseLibraryRepository courseLibraryRepository, IMapper mapper)
+        {
+            _courseLibraryRepository = courseLibraryRepository ??
+                                       throw new ArgumentNullException(nameof(courseLibraryRepository));
+            _mapper = mapper ?? throw new ArgumentNullException(nameof(mapper));
+        }
+
+        public ActionResult<IEnumerable<AuthorDto>> CreateAuthorCollection(IEnumerable<AuthorForCreationDto> authorCollection)
+        {
+            var authorEntities = _mapper.Map<IEnumerable<Entities.Author>>(authorCollection);
+            foreach (var author in authorEntities)
+            {
+                _courseLibraryRepository.AddAuthor(author);
+            }
+
+            _courseLibraryRepository.Save();
+            
+            return Ok();
+        }
+    }
+}
