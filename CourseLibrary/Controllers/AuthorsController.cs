daf2e184bc8a59208b498563999c7da53b990c50
Mustafa GUNES <gunes149@gmail.com>
2020-09-16 09:11:47 +0300

    added filtering and searching

--- CourseLibrary/Controllers/AuthorsController.cs
+++ CourseLibrary/Controllers/AuthorsController.cs
@@ -23,9 +23,9 @@ namespace CourseLibrary.API.Controllers
         
         [HttpGet()]
         [HttpHead]
-        public ActionResult<IEnumerable<AuthorDto>> GetAuthors()
+        public ActionResult<IEnumerable<AuthorDto>> GetAuthors(string mainCategory, string searchQuery)
         {
-            var authorsFromRepo = _courseLibraryRepository.GetAuthors();
+            var authorsFromRepo = _courseLibraryRepository.GetAuthors(mainCategory, searchQuery);
             var authors = _mapper.Map<IEnumerable<AuthorDto>>(authorsFromRepo);
             return Ok(authors);
         }
