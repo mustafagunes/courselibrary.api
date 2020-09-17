5023acc1fce46b6265d7813edad806a40cf99d9d
Mustafa GUNES <gunes149@gmail.com>
2020-09-17 14:45:52 +0300

    addeed custom validation file and used

--- CourseLibrary/Models/CourseForCreationDto.cs
+++ CourseLibrary/Models/CourseForCreationDto.cs
@@ -1,10 +1,12 @@
 using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using CourseLibrary.API.ValidationAttributes;
 
 namespace CourseLibrary.API.Models
 {
-    public class CourseForCreationDto : IValidatableObject
+    [CourseTitleDescriptionMustBeDifferentDescription]
+    public class CourseForCreationDto
     {
         [Required]
         [MaxLength(100)]
@@ -12,16 +14,5 @@ namespace CourseLibrary.API.Models
         
         [MaxLength(1500)]
         public string Description { get; set; }
-
-        public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
-        {
-            if (Title == Description)
-            {
-                yield return new ValidationResult(
-                    "The provided description should be different from the title.",
-                    new[] {"CourseForCreationDto"}
-                );
-            }
-        }
     }
 }
